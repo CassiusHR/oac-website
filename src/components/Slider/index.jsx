4b3d232e2311d58a60dcbcd90004d08fde8b231f
Carlos Henriquez <fog.cassius@gmail.com>
2019-06-24 16:46:34 -0400

    nav advanced, slider started

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick'
+
+const MainSlider = () => {
+  return(
+    <div>
+      hola
+    </div>
+  )
+}
+export default MainSlider
