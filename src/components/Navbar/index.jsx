6352496fa46cb9c39bc2b1dcbe0d5dc4d7aa7413
Carlos Henriquez <fog.cassius@gmail.com>
2019-06-28 15:51:51 -0400

    added mobile hamburger button managed by local state

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,10 @@
-import React,{ useEffect } from 'react'
+import React,{ useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import anime from 'animejs'
 import './style.scss'
 
 const Navbar = () => {
+  const  [ isOpen, setisOpen ] = useState(false);
 
   useEffect(()=>{
     anime.timeline({
@@ -29,6 +30,9 @@ const Navbar = () => {
     })
   },[])
 
+  const handleClick = ()=>{
+    isOpen === true ? setisOpen(false) : setisOpen(true)
+  }
   return (
     <nav className="main-nav">
       <div className="logo-container">
@@ -38,9 +42,20 @@ const Navbar = () => {
         <NavLink to="/" exact activeClassName="active"><li className="nav-link">Inicio</li></NavLink>
         <NavLink to="/blog" activeClassName="active"><li className="nav-link">Blog</li></NavLink>
         <NavLink to="/quienes-somos" activeClassName="active"><li className="nav-link">Quienes somos</li></NavLink>
-        <li className="nav-link">Cartelera</li>
-        <li className="nav-link">Contacto</li>
+        <NavLink to="/cartelera" activeClassName="active"><li className="nav-link">Cartelera</li></NavLink>
+        <NavLink to="/contacto" activeClassName="active"><li className="nav-link">Contacto</li></NavLink>
       </ul>
+      <div className={isOpen === true ? 'mobile-nav-icon-container active':'mobile-nav-icon-container'} onClick={handleClick}>
+          <div className={isOpen === true ? 'mobile-nav-hamburger mob-nav-toggle cross':'mobile-nav-hamburger mob-nav-toggle'} id="MobileNavToggle">
+              <svg viewBox="0 0 800 600">
+                  <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200"
+                      id="top"></path>
+                  <path d="M300,320 L540,320" id="middle"></path>
+                  <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190"
+                      id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
+              </svg>
+          </div>
+      </div>
     </nav>
   )
 }
