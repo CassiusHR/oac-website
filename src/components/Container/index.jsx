41de85f3aa0c40bfd95285c81798f5325657edc7
Carlos Henriquez <fog.cassius@gmail.com>
2019-07-02 14:58:48 -0400

    menu behavior polished

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+import anime from 'animejs'
+import './style.scss'
+
+const Container = () =>{
+  return(
+    <div className="main-wrap">
+      <div className="main-container">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Container
